a3d23d43e0578e99a0b30652b2b303c90affee12
Aga <agapoli@gmail.com>
2022-07-10 15:14:42 +0200

    Add Nav component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { BrowserRouter, Routes, Route, } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
 
 import HomePage from './pages/HomePage/HomePage';
 import Products from './pages/Products/Products';
 import Cart from './pages/Cart/Cart';
 import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 
 import './App.scss';
@@ -14,6 +15,7 @@ function App() {
     <div className="App"> 
       <BrowserRouter>
         <Header />
+        <Nav />
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route path="/products" element={<Products/>} />
