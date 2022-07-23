7badc6287501d3ae25d7f027e20e69dc38a729b2
Sionna Poleszuk <sionna.pol@gmail.com>
2022-07-23 21:36:52 +0200

    header-styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './Header.scss';
-import logo from '../../images/logo1.png'
+import logo from '../../images/logo1.png';
+import { IconContext } from "react-icons";
+import {BsCartFill} from 'react-icons/bs';
+import {AiOutlineUser} from 'react-icons/ai';
+
 
 
 export default function Header() {
   return (
     <header>
-      <img className='logo'src={logo} alt='logo' />
-      <div>cart</div>
-      <div>login</div>
-      <div>search</div>
+      <img className='logo' src={logo} alt='logo' />
+      <IconContext.Provider value={{className: "icons" }}>
+        <BsCartFill />
+        <AiOutlineUser/>
+      </IconContext.Provider>
+      <form>
+        <input type="text" id="search"  placeholder="szukaj " autoComplete="off"/>
+      </form>
     </header>
   )
 }
